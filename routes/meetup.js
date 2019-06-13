fde08612733c5122c046cf5bb906fe69253641bd
Vasiliy Knizhnikov <knizhnikov@Vasiliys-MacBook-Pro.local>
2019-06-13 13:52:26 +0300

    several additions

--- routes/meetup.js
+++ routes/meetup.js
@@ -1,22 +1,31 @@
-var express = require('express');
+const express = require('express');
 const axios = require('axios');
-var router = express.Router();
+const router = express.Router();
+
+const groupName = 'GDG-Astrakhan';
 
 router.get('/events', async function (req, res, next) {
-
-    let response = {data: {}};
-    let groupName = 'GDG-Astrakhan';
-
     try {
         const url = `https://api.meetup.com/${groupName}/events?photo-host=public&scroll=future_or_past&fields=featured_photo&has_ended=true&sign=true&key=${process.env.MEETUP_API_KEY}`;
         response = await axios.get(url);
-    } catch (e){
-        res.send(e);
+        res.send(response.data);
+    } catch (e) {
+        res.sendStatus(500);
+        console.log(e);
         return;
     }
-    // const mock = require('./events-mock');
+});
 
-    res.send(response.data);
+router.get('/members', async function (req, res, next) {
+    try {
+        const url = `https://api.meetup.com/2/members?page=9&photo-host=public&group_urlname=${groupName}&sign=true&key=${process.env.MEETUP_API_KEY}`;
+        response = await axios.get(url);
+        res.send(response.data);
+    } catch (e) {
+        res.sendStatus(500);
+        console.log(e);
+        return;
+    }
 });
 
 module.exports = router;
